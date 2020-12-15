dc33db7bb74d1b20700251a40706b4847be45bf8
Indah Puspita <indahpuspitaa17@apps.ipb.ac.id>
2020-12-15 21:41:05 +0700

    set client side rendering and axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,61 +1,61 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
-    // Global page headers (https://go.nuxtjs.dev/config-head)
-    head: {
-        titleTemplate: '%s - bukukas',
-        title: 'bukukas',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-
-    // Global CSS (https://go.nuxtjs.dev/config-css)
-    css: [],
-
-    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: [],
-
-    // Auto import components (https://go.nuxtjs.dev/config-components)
-    components: true,
-
-    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-    buildModules: [
-        // https://go.nuxtjs.dev/vuetify
-        '@nuxtjs/vuetify',
-        '@nuxtjs/bootstrap-vue'
+  ssr: false,
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    titleTemplate: "%s | Bukukas",
+    title: "Dashboard",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Website Bukukas" },
     ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
 
-    // Modules (https://go.nuxtjs.dev/config-modules)
-    modules: [
-        '@nuxtjs/axios',
-        '@nuxtjs/auth-next'
-    ],
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [],
 
-    // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
-    vuetify: {
-        customVariables: ['~/assets/variables.scss'],
-        theme: {
-            dark: false,
-            themes: {
-                dark: {
-                    primary: colors.blue.darken2,
-                    accent: colors.grey.darken3,
-                    secondary: colors.amber.darken3,
-                    info: colors.teal.lighten1,
-                    warning: colors.amber.base,
-                    error: colors.deepOrange.accent4,
-                    success: colors.green.accent3
-                }
-            }
-        }
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/vuetify
+    "@nuxtjs/vuetify",
+    "@nuxtjs/bootstrap-vue",
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth-next"],
+
+  axios: {
+    baseURL: process.env.BACKEND_URL || "http://localhost:3333",
+  },
+
+  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
+  vuetify: {
+    customVariables: ["~/assets/variables.scss"],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
     },
+  },
 
-    // Build Configuration (https://go.nuxtjs.dev/config-build)
-    build: {}
-}
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+};
